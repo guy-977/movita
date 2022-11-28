b49175d65995f4f90d849e3602b55c97e386a999
Ahmed Al-rufai <86237598+guy-977@users.noreply.github.com>
2022-11-28 21:59:15 +0300

    adding API key
    I've generated an API key from themoviedb.org , and now I'm updating APILINK and SEARCHAPI constants with my own API key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1'
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=39637281ff4d0f7a15dfb6417aec37dc&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query='
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=39637281ff4d0f7a15dfb6417aec37dc&query='
 
